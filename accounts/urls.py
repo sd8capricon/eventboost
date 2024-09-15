7183b525c3505a9a88b4b949875e38ca1485571c
Siddharth Dhaigude <sd8capricon@gmail.com>
2024-09-15 23:29:23 +0530

    furni template

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name=""),
     path("signup/", views.register_user, name="signup"),
     path("login/", views.login_user, name="login"),
     path("logout_user/", views.logout_user, name="logout"),
--- eventboost/urls.py
+++ eventboost/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from eventboost.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index, name="index"),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
     path("events/", include("events.urls")),
