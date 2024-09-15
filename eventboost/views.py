19952b15bed8028051e83797bbc341d2ce5c27ab
Siddharth Dhaigude <sd8capricon@gmail.com>
2024-09-16 01:52:08 +0530

    index event cards

--- eventboost/views.py
+++ eventboost/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from events.models import Event
 
 
 def index(request):
-    return render(request, "index.html")
+    events = Event.objects.order_by("pk")[:3]
+    return render(request, "index.html", {"events": events})
